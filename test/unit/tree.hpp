2ee63df21e43210152d339e9eed47e530f8c6725
Dominik Kaszewski <dakaszewski@gmail.com>
2025-11-07 21:16:55 +0100

    Use new API in test_filesystem_explorer

--- test/unit/tree.hpp
+++ test/unit/tree.hpp
@@ -8,36 +8,6 @@
 
 namespace glug::unit_test {
 
-namespace old {
-
-class tree {
-    public:
-    static std::filesystem::path temp_dir();
-
-    tree(const char* path);
-    tree(const char* path, const char* contents);
-    tree(const char* path, const std::vector<tree>& contents);
-
-    std::filesystem::directory_entry create() const;
-    void remove() const;
-
-    const std::filesystem::path name() const { return path.filename(); }
-    std::vector<std::filesystem::path> list() const;
-
-    friend bool operator==(const tree& lhs, const tree& rhs);
-    friend bool operator<(const tree& lhs, const tree& rhs);
-    friend std::ostream& operator<<(std::ostream& os, const tree& obj);
-
-    private:
-    std::filesystem::path path{};
-    bool is_dir{};
-    std::variant<std::string, std::vector<tree>> inner{};
-};
-
-inline tree operator""_t(const char* path, size_t) { return tree{ path }; }
-
-}  // namespace old
-
 class node;
 class temp_fs;
 

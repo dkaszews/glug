6a6953ef5846a6de93fcda0cc6e0d3b749f18560
Dominik Kaszewski <dakaszewski@gmail.com>
2026-01-28 19:38:25 +0100

    Forward print backport to stdlib if available

--- include/glug/detail/backport/print.hpp
+++ include/glug/detail/backport/print.hpp
@@ -1,6 +1,42 @@
 // Provided as part of glug under MIT license, (c) 2026 Dominik Kaszewski
 #pragma once
 
+#include <version>
+
+#if defined(__cpp_lib_print) && __cpp_lib_print >= 202207L
+
+#include <print>
+
+namespace glug::backport {
+
+template <typename... ARGS>
+void print(
+        std::FILE* stream, std::format_string<ARGS...> format, ARGS&&... args
+) {
+    ::std::print(stream, format, std::forward<ARGS>(args)...);
+}
+
+template <typename... ARGS>
+void print(std::format_string<ARGS...> format, ARGS&&... args) {
+    ::std::print(format, std::forward<ARGS>(args)...);
+}
+
+template <typename... ARGS>
+void println(
+        std::FILE* stream, std::format_string<ARGS...> format, ARGS&&... args
+) {
+    ::std::println(stream, format, std::forward<ARGS>(args)...);
+}
+
+template <typename... ARGS>
+void println(std::format_string<ARGS...> format, ARGS&&... args) {
+    ::std::println(format, std::forward<ARGS>(args)...);
+}
+
+}  // namespace glug::backport
+
+#else
+
 #include <format>
 #include <iostream>
 
@@ -11,7 +47,6 @@ template <typename... ARGS>
 void print(
         std::FILE* stream, std::format_string<ARGS...> format, ARGS&&... args
 ) {
-
     std::fputs(
             std::format(format, std::forward<ARGS>(args)...).c_str(), stream
     );
@@ -36,4 +71,6 @@ void println(std::format_string<ARGS...> format, ARGS&&... args) {
 }
 
 }  // namespace glug::backport
+
+#endif
 

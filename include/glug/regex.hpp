8797de75ee04700cf3bd7055af9655293bf2492f
Dominik Kaszewski <dakaszewski@gmail.com>
2026-05-18 12:23:37 +0200

    Document regex

--- include/glug/regex.hpp
+++ include/glug/regex.hpp
@@ -4,17 +4,57 @@
 #include <memory>
 #include <string_view>
 
+/**
+ * Regular expression utilities.
+ */
 namespace glug::regex {
 
+/**
+ * Wrapper around regex engine, which can be provided by either STL
+ * or third-party libraries, depending on project config.
+ */
 class engine {
     public:
+    /*
+     * Default-construct a regex matcher, which does not match anything.
+     *
+     * @todo: UT if this is true, might actually be UB.
+     */
     engine() = default;
+
+    /**
+     * Construct regex matcher from given regex.
+     *
+     * @param pattern regular expression
+     * @throws std::exception if pattern is not a valid regex.
+     *
+     * @todo: Add custom exception class.
+     * @todo: UT exceptions.
+     */
     explicit engine(std::string_view pattern);
 
+    /**
+     * Check given string against the matcher.
+     *
+     * @param s string for matching against the regex.
+     * @return True if entire string matches, false otherwise.
+     */
     [[nodiscard]] bool match(std::string_view s) const;
+
+    /**
+     * Alias for `engine::match(std::string_view)`.
+     *
+     * @param s string for matching against the regex.
+     * @return True if entire string matches, false otherwise.
+     *
+     * @see engine::match(std::string_view)
+     */
     [[nodiscard]] bool operator()(std::string_view s) const { return match(s); }
 
+    // TODO: #90 - Generate version header
+    /// @cond 0
     static std::string_view license();
+    /// @endcond
 
     private:
     struct impl;
